8e2490900d7a08d7d8d2ab55d7ac61d75984b7ab
adamhenson <ahenson@paperlesspost.com>
2019-09-26 15:31:04 -0400

    initial.

---
+++ README.md
@@ -0,0 +1,57 @@
+# Lighthouse Check Results Action
+
+This action is used with [Lighthouse Check action](https://github.com/foo-software/lighthouse-check-action) to determine status of running workflow. **Its main purpose is to fail the workflow if Lighthouse scores don't meet minimum requirements** as defined by inputs.
+
+## Inputs
+
+Each value of the inputs below can be used alone or together. If a score of any Lighthouse audit ran by [Lighthouse Check action](https://github.com/foo-software/lighthouse-check-action) is lower than the value - the workflow will fail with an error message detailing which failed and scores.
+
+<table>
+  <tr>
+    <th>Name</th>
+  </tr>
+  <tr>
+    <td><code>minAccessibilityScore</code></td>
+  </tr>
+  <tr>
+    <td><code>minBestPracticesScore</code></td>
+  </tr>
+  <tr>
+    <td><code>minPerformanceScore</code></td>
+  </tr>
+  <tr>
+    <td><code>minProgressiveWebAppScore</code></td>
+  </tr>
+  <tr>
+    <td><code>minSeoScore</code></td>
+  </tr>
+</table>
+
+## Example usage
+
+In the below example we use [Lighthouse Check action](https://github.com/foo-software/lighthouse-check-action) to run audits and provide results as an output that we utilize in a following step.
+
+```yaml
+name: Test Lighthouse Check Status
+on: [push]
+
+jobs:
+  lighthouse-check:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@master
+    - name: Run Lighthouse
+      id: lighthouseCheck
+      uses: foo-software/lighthouse-check-action@master
+      with:
+        urls: 'https://www.foo.software,https://www.foo.software/contact'
+    - name: Handle Lighthouse Check results
+      uses: ./
+      with:
+        lighthouse-check-results: ${{ steps.lighthouseCheck.outputs.lighthouseCheckResults }}
+        minAccessibilityScore: 90
+        minBestPracticesScore: 50
+        minPerformanceScore: 50
+        minProgressiveWebAppScore: 50
+        minSeoScore: 50
+```
---
+++ index.js
@@ -0,0 +1,137 @@
+const core = require('@actions/core');
+
+const normalizeInput = input => {
+  if (input === 'true') {
+    return true;
+  }
+
+  if (input === 'false') {
+    return false;
+  }
+
+  if (input === '') {
+    return undefined;
+  }
+
+  return input;
+};
+
+const getScoreFailMessage = ({
+  name,
+  url,
+  minScore,
+  score
+}) => {
+  // if inputs are not specified - assume we shouldn't fail
+  if (!minScore || !score) {
+    return [];
+  }
+
+  if (Number(score) < Number(minScore)) {
+    return [`${url}: ${name}: minimum score: ${minScore}, actual score: ${score}`];
+  }
+
+  return [];
+};
+
+const getFailureMessages = ({
+  minAccessibilityScore,
+  minBestPracticesScore,
+  minPerformanceScore,
+  minProgressiveWebAppScore,
+  minSeoScore,
+  results
+}) => {
+  return results.data.reduce((accumulator, current) => ([
+    ...accumulator,
+    ...(getScoreFailMessage({
+      name: 'Accessibility',
+      minScore: minAccessibilityScore,
+      score: current.scores.accessibility,
+      ...current
+    })),
+    ...(getScoreFailMessage({
+      name: 'Best Practices',
+      minScore: minBestPracticesScore,
+      score: current.scores.bestPractices,
+      ...current
+    })),
+    ...(getScoreFailMessage({
+      name: 'Performance',
+      minScore: minPerformanceScore,
+      score: current.scores.performance,
+      ...current
+    })),
+    ...(getScoreFailMessage({
+      name: 'Progressive Web App',
+      minScore: minProgressiveWebAppScore,
+      score: current.scores.progressiveWebApp,
+      ...current
+    })),
+    ...(getScoreFailMessage({
+      name: 'SEO',
+      minScore: minSeoScore,
+      score: current.scores.seo,
+      ...current
+    }))
+  ]), []);
+}
+
+  try {
+    const urls = normalizeInput(core.getInput('urls'));
+
+    const minAccessibilityScore = normalizeInput(core.getInput('minAccessibilityScore'));
+    const minBestPracticesScore = normalizeInput(core.getInput('minBestPracticesScore'));
+    const minPerformanceScore = normalizeInput(core.getInput('minPerformanceScore'));
+    const minProgressiveWebAppScore = normalizeInput(core.getInput('minProgressiveWebAppScore'));
+    const minSeoScore = normalizeInput(core.getInput('minSeoScore'));
+
+    const results = await lighthouseCheck({
+      author: normalizeInput(core.getInput('author')),
+      apiToken: normalizeInput(core.getInput('apiToken')),
+      awsAccessKeyId: normalizeInput(core.getInput('awsAccessKeyId')),
+      awsBucket: normalizeInput(core.getInput('awsBucket')),
+      awsRegion: normalizeInput(core.getInput('awsRegion')),
+      awsSecretAccessKey: normalizeInput(core.getInput('awsSecretAccessKey')),
+      branch: normalizeInput(core.getInput('branch')),
+      configFile: normalizeInput(core.getInput('configFile')),
+      emulatedFormFactor: normalizeInput(core.getInput('emulatedFormFactor')),
+      locale: normalizeInput(core.getInput('locale')),
+      help: normalizeInput(core.getInput('help')),
+      outputDirectory: normalizeInput(core.getInput('outputDirectory')),
+      pr: normalizeInput(core.getInput('pr')),
+      sha: normalizeInput(core.getInput('sha')),
+      slackWebhookUrl: normalizeInput(core.getInput('slackWebhookUrl')),
+      tag: normalizeInput(core.getInput('tag')),
+      timeout: normalizeInput(core.getInput('timeout')),
+      throttling: normalizeInput(core.getInput('throttling')),
+      throttlingMethod: normalizeInput(core.getInput('throttlingMethod')),
+      urls: !urls ? undefined : urls.split(','),
+      verbose: normalizeInput(core.getInput('verbose')),
+      wait: normalizeInput(core.getInput('wait'))
+    });
+
+    console.log('results', results);
+
+    // if we need to fail when scores are too low...
+    if (minAccessibilityScore || minBestPracticesScore
+      || minPerformanceScore || minProgressiveWebAppScore || minSeoScore) {
+      const failures = getFailureMessages({
+        minAccessibilityScore,
+        minBestPracticesScore,
+        minPerformanceScore,
+        minProgressiveWebAppScore,
+        minSeoScore,
+        results
+      });
+
+      // if we have scores that were below the minimum requirement
+      if (failures.length) {
+        // comma-separate error messages and remove the last comma
+        const failureMessage = failures.join('\n');
+        throw new Error(`Minimum score requirements failed:\n${failureMessage}`);
+      }
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
