eda43dbf32a7ce5f1eab555ce04fb27d3a32a98f
Adam <adamhenson1979@gmail.com>
2019-09-29 00:56:11 -0400

    chore: validate status from NPM module - 2 (#3)
    * chore: validate status from NPM module.
    
    * chore: dependencies for Node 12 :(
    
    * chore: revert version.
    
    * chore: dependencies
    
    * chore: async
    
    * chore: async
    
    * chore: revert
    
    * fix: dependency update.

--- index.js
+++ index.js
@@ -9,40 +9,42 @@ const formatInput = input => {
   return input;
 };
 
-try {
-  const minAccessibilityScore = formatInput(
-    core.getInput('minAccessibilityScore')
-  );
-  const minBestPracticesScore = formatInput(
-    core.getInput('minBestPracticesScore')
-  );
-  const minPerformanceScore = formatInput(
-    core.getInput('minPerformanceScore')
-  );
-  const minProgressiveWebAppScore = formatInput(
-    core.getInput('minProgressiveWebAppScore')
-  );
-  const minSeoScore = formatInput(core.getInput('minSeoScore'));
-  const results = JSON.parse(core.getInput('lighthouseCheckResults'));
+(async () => {
+  try {
+    const minAccessibilityScore = formatInput(
+      core.getInput('minAccessibilityScore')
+    );
+    const minBestPracticesScore = formatInput(
+      core.getInput('minBestPracticesScore')
+    );
+    const minPerformanceScore = formatInput(
+      core.getInput('minPerformanceScore')
+    );
+    const minProgressiveWebAppScore = formatInput(
+      core.getInput('minProgressiveWebAppScore')
+    );
+    const minSeoScore = formatInput(core.getInput('minSeoScore'));
+    const results = JSON.parse(core.getInput('lighthouseCheckResults'));
 
-  // if we need to fail when scores are too low...
-  if (
-    minAccessibilityScore ||
-    minBestPracticesScore ||
-    minPerformanceScore ||
-    minProgressiveWebAppScore ||
-    minSeoScore
-  ) {
-    validateStatus({
-      minAccessibilityScore,
-      minBestPracticesScore,
-      minPerformanceScore,
-      minProgressiveWebAppScore,
-      minSeoScore,
-      results,
-      verbose: true
-    });
+    // if we need to fail when scores are too low...
+    if (
+      minAccessibilityScore ||
+      minBestPracticesScore ||
+      minPerformanceScore ||
+      minProgressiveWebAppScore ||
+      minSeoScore
+    ) {
+      await validateStatus({
+        minAccessibilityScore,
+        minBestPracticesScore,
+        minPerformanceScore,
+        minProgressiveWebAppScore,
+        minSeoScore,
+        results: results.data,
+        verbose: true
+      });
+    }
+  } catch (error) {
+    core.setFailed(error.message);
   }
-} catch (error) {
-  core.setFailed(error.message);
-}
+})();
